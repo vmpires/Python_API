4aea0fa7952bcb656df72a727cd0a1702925eb6f
Victor Pires <vctprs@gmail.com>
2021-11-03 18:14:00 -0300

    Extracting data

--- 10) FutAPI.py
+++ 10) FutAPI.py
@@ -7,12 +7,8 @@ futapikeytest = key['futapikeytest']
 futapikeylive = key['futapikeylive']
 campeonato_id = 10
 urlroot = "https://api.api-futebol.com.br"
-head = {'Authorization': 'Bearer ' + futapikeytest}
+head = {'Authorization': 'Bearer ' + futapikeylive}
 data = {'app' : 'aaaaa'}
-
-url = urlroot + f'/v1/campeonatos/{campeonato_id}'
-response = requests.post(url, json=data, headers=head)
-resposta = response.json()
 
 dictpart1 = {
     "Id_Partida": [],
@@ -33,7 +29,7 @@ dictpart1 = {
     "Dribes_visitante": [],
 }
 
-for rodada in range(1,2):
+for rodada in range(1,11):
     url = urlroot + f'/v1/campeonatos/{campeonato_id}/rodadas/{rodada}'
     response = requests.post(url, json=data, headers=head)
     rodada = response.json()
@@ -42,26 +38,23 @@ for rodada in range(1,2):
         urlpartida = urlroot + f"/v1/partidas/{partida['partida_id']}"
         response = requests.post(urlpartida, json=data, headers=head)
         resposta = response.json()
-        dictpart1['Id_Partida'] = resposta['partida_id']
-        dictpart1['Rodada'] = resposta['campeonato']['rodada_atual']['nome']
-        dictpart1['Placar'] = resposta['placar']
-        dictpart1['Time_Mandante'] = resposta['time_mandante']['nome_popular']
-        dictpart1['Time_Visitante'] = resposta['time_visitante']['nome_popular']
-        dictpart1['Gols_Mandante'] = resposta['placar_mandante']
-        dictpart1['Gols_Visitante'] = resposta['placar_visitante']
-        dictpart1['Data_Realizacao'] = resposta['data_realizacao']
-        dictpart1['Hora_Realizacao'] = resposta['hora_realizacao']
-        dictpart1['Estadio'] = resposta['estadio']["nome_popular"]
-        dictpart1['Posse_de_bola_mandante'] = resposta['estatisticas']['mandante']['posse_de_bola']
-        dictpart1['Finalizacao_mandante'] = resposta['estatisticas']['mandante']['finalizacao']['total']
-        dictpart1['Dribes_mandante'] = resposta['estatisticas']['mandante']['dribes_bem_sucedidos']
-        dictpart1['Posse_de_bola_visitante'] = resposta['estatisticas']['visitante']['posse_de_bola']
-        dictpart1['Finalizacao_visitante'] = resposta['estatisticas']['visitante']['finalizacao']['total']
-        dictpart1['Dribes_visitante'] = resposta['estatisticas']['visitante']['posse_de_bola']
+        dictpart1['Id_Partida'].append(resposta['partida_id'])
+        dictpart1['Rodada'].append(resposta['campeonato']['rodada_atual']['nome'])
+        dictpart1['Placar'].append(resposta['placar'])
+        dictpart1['Time_Mandante'].append(resposta['time_mandante']['nome_popular'])
+        dictpart1['Time_Visitante'].append(resposta['time_visitante']['nome_popular'])
+        dictpart1['Gols_Mandante'].append(resposta['placar_mandante'])
+        dictpart1['Gols_Visitante'].append(resposta['placar_visitante'])
+        dictpart1['Data_Realizacao'].append(resposta['data_realizacao'])
+        dictpart1['Hora_Realizacao'].append(resposta['hora_realizacao'])
+        dictpart1['Estadio'].append(resposta['estadio']["nome_popular"])
+        dictpart1['Posse_de_bola_mandante'].append(resposta["estatisticas"]["mandante"]["posse_de_bola"])
+        dictpart1['Finalizacao_mandante'].append(resposta["estatisticas"]["mandante"]['finalizacao']['total'])
+        dictpart1['Dribes_mandante'].append(resposta["estatisticas"]["mandante"]['dribes_bem_sucedidos'])
+        dictpart1['Posse_de_bola_visitante'].append(resposta["estatisticas"]["visitante"]["posse_de_bola"])
+        dictpart1['Finalizacao_visitante'].append(resposta["estatisticas"]["visitante"]['finalizacao']['total'])
+        dictpart1['Dribes_visitante'].append(resposta["estatisticas"]["visitante"]["posse_de_bola"])
 
 df = pd.DataFrame(dictpart1, index=[0])
-
-df.to_excel("parte1.xlsx", sheet_name="tabestbr2021", index=False)
-
 
     
